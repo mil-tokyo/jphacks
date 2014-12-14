f3238d7746e1a7cdf254206f84a94e1dbb03f3d5
kanehira <kanehira@mi.t.u-tokyo.ac.jp>
2014-12-14 16:18:17 +0900

    change

--- sample_data/generate_sample_data.py
+++ sample_data/generate_sample_data.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def sample_kmeans():
-    priors = np.random.normal(loc=[0, 0], size=(5, 2), scale=1.2)
+    priors = np.random.normal(loc=[0, 0], size=(5, 2), scale=0.8)
     print priors
     f=open("sample_kmeans.csv","w")
     for prior in priors:
@@ -10,14 +10,13 @@ def sample_kmeans():
         for data in a:
             f.write("{},{},{}\n".format(0, data[0], data[1]))
     f.close()
-        
 
-
+    
 def sample_svm():
     pos_mean = np.array([-0.5, -0.5])
     neg_mean = np.array([0.5, 0.5])
-    pos = np.random.normal(loc=pos_mean, size=(50,2))
-    neg = np.random.normal(loc=neg_mean, size=(50,2))
+    pos = np.random.normal(loc=pos_mean, size=(300,2), scale=0.5)
+    neg = np.random.normal(loc=neg_mean, size=(300,2), scale=0.5)
     f=open("sample_svm.csv","w")
     for ind, samples in enumerate([neg, pos]):
         for data in  samples:
