fe25e242e1e1a835da20fdd0f0f666dc175b4574
kanehira <kanehira@mi.t.u-tokyo.ac.jp>
2014-12-14 16:08:36 +0900

    add samples

---
+++ sample_data/generate_sample_data.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def sample_kmeans():
+    priors = np.random.normal(loc=[0, 0], size=(5, 2), scale=1.2)
+    print priors
+    f=open("sample_kmeans.csv","w")
+    for prior in priors:
+        a = np.random.normal(loc=prior, size=(50, 2), scale=0.3)
+        for data in a:
+            f.write("{},{},{}\n".format(0, data[0], data[1]))
+    f.close()
+        
+
+
+def sample_svm():
+    pos_mean = np.array([-0.5, -0.5])
+    neg_mean = np.array([0.5, 0.5])
+    pos = np.random.normal(loc=pos_mean, size=(50,2))
+    neg = np.random.normal(loc=neg_mean, size=(50,2))
+    f=open("sample_svm.csv","w")
+    for ind, samples in enumerate([neg, pos]):
+        for data in  samples:
+            f.write("{},{},{}\n".format(ind, data[0], data[1]))
+    f.close()
+
+
+sample_svm()
